0d556d8c0efafe9e13549da22c356fc7f4f922fc
wyqiand <1114640680@qq.com>
2020-06-09 00:21:56 +0800

    新增客户管理->公共保额管理

---
+++ src/api/client/publicCoverage.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function getList(data) {
+  return request({
+    url: '/base/publicCoverage/list',
+    method: 'post',
+    data
+  })
+}
+
+export function findById(id) {
+  return request({
+    url: '/base/publicCoverage/' + id,
+    method: 'get'
+  })
+}
+
+export function save(data) {
+  return request({
+    url: '/base/publicCoverage/save',
+    method: 'post',
+    data
+  })
+}
+
+export function del(id) {
+  return request({
+    url: '/base/publicCoverage/' + id,
+    method: 'delete'
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: '/base/publicCoverage/edit',
+    method: 'put',
+    data
+  })
+}
+
---
+++ src/views/client/publicCoverage/index.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="app-container">
+    <el-card>
+      <div>
+        <el-input v-model="listQuery.teamNo" style="width: 200px;" placeholder="请输入集团/团体号称查询" />
+        <el-input v-model="listQuery.teamTyp" style="width: 200px;" placeholder="请选择集团/团体类型查询" />
+        <el-input v-model="listQuery.pubCoverTyp" style="width: 200px;" placeholder="请选择公共保额类型查询" />
+        <el-button style="margin-left: 10px;" type="success" icon="el-icon-search" @click="fetchData">查询</el-button>
+        <el-button style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleSave">添加</el-button>
+      </div>
+      <br>
+      <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" border fit highlight-current-row>
+        <el-table-column align="center" label="ID" width="95">
+          <template slot-scope="scope">
+            {{ scope.row.id }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="公共保额类型" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.pubCoverTyp }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="集团/团体类型" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.teamTyp }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="集团/团体号" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.teamNo }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="起始日" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.startTm }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="终止日" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.endTm }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="公共保额总金额" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.pubCoverLimit }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="产品" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.product }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="已用公共保额" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.pubCoverUsed }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="公共保额说明" width="150">
+          <template slot-scope="scope">
+            {{ scope.row.pubCoverDesc }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="操作" fixed="right">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" icon="el-icon-edit" @click="handleEdit(scope.row.id)">编辑</el-button>
+            <el-button type="danger" size="mini" icon="el-icon-delete" class="action-button" @click="handleDel(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <save :son-data="form" @sonStatus="status" />
+
+      <pagination
+        v-show="total>0"
+        :total="total"
+        :page.sync="listQuery.pageNum"
+        :limit.sync="listQuery.pageSize"
+        @pagination="fetchData"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { getList, findById, del } from '@/api/client/publicCoverage'
+import Pagination from '@/components/Pagination'
+import Save from './save'
+
+export default {
+  components: { Pagination, Save },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      listQuery: {
+        pageNum: 1,
+        pageSize: 10,
+        teamTyp: '',
+        teamNo: '',
+        pubCoverTyp: '',
+        sort: '+id'
+      },
+      total: 0,
+      dialogVisible: false,
+      form: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    _notify(message, type) {
+      this.$message({
+        message: message,
+        type: type
+      })
+    },
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        this.list = response.data.data
+        this.total = response.data.total
+        this.listLoading = false
+      })
+    },
+    handleSave() {
+      this.form = { id: null }
+      this.dialogVisible = true
+    },
+    handleEdit(id) {
+      // 跳转到新的页面
+      findById(id).then(response => {
+        this.form = response.data
+      })
+    },
+
+    // 子组件的状态Flag，子组件通过`this.$emit('sonStatus', val)`给父组件传值
+    // 父组件通过`@sonStatus`的方法`status`监听到子组件传递的值
+    status(data) {
+      if (data) {
+        this.fetchData()
+      }
+    },
+
+    handleDel(id) {
+      this.$confirm('你确定永久删除此数据？, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        del(id).then(response => {
+          if (response.code === 200) {
+            this._notify(response.msg, 'success')
+          } else {
+            this._notify(response.msg, 'error')
+          }
+          this.fetchData()
+        })
+      }).catch(() => {
+        this._notify('已取消删除', 'info')
+      })
+    }
+  }
+}
+</script>
