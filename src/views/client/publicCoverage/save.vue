6cc26b51b25832c8b3ed880772a8f99d3f2639aa
wyqiand <1114640680@qq.com>
2020-06-12 14:08:10 +0800

    下拉支持多个key

--- src/views/client/publicCoverage/save.vue
+++ src/views/client/publicCoverage/save.vue
@@ -104,7 +104,6 @@ export default {
   watch: {
     'sonData': function(newVal, oldVal) {
       this.form = newVal
-      this.imgURL = this.form.avatar
       this.dialogVisible = true
       if (newVal.id != null) {
         this.dialogTitle = 'Edit'
