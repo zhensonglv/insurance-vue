24b2710180e52bedaefcf9c4421f41eaca1bc861
maycsimida <173626940@qq.com>
2020-06-23 01:31:55 +0800

    社保地页面增加

---
+++ src/views/param/socialinsuAddr/save.vue
@@ -0,0 +1,146 @@
+<template>
+  <el-dialog :title="dialogTitle" :before-close="handleClose" :visible.sync="dialogVisible" width="55%">
+    <el-form ref="form" :inline="true" :rules="rules" :model="form" status-icon label-position="right" label-width="80px">
+      <el-form-item label="社保地区号" prop="sociInsuAreaNo" label-width="120px">
+        <el-input v-model="form.sociInsuAreaNo" placeholder="请输入社保地区号" />
+      </el-form-item>
+      <el-form-item label="社保地区" prop="sociInsuArea" label-width="120px">
+        <el-input v-model="form.sociInsuArea" placeholder="请输入社保地区" />
+      </el-form-item>
+      <el-form-item label="社保类型" prop="sociInsuTyp" label-width="120px">
+        <el-input v-model="form.sociInsuTyp" placeholder="请输入社保类型" />
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="handleClose">
+        Cancel
+      </el-button>
+      <el-button type="primary" @click="onSubmit('form')">
+        Confirm
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { save, edit } from '@/api/base'
+
+export default {
+  // 父组件向子组件传值，通过props获取。
+  // 一旦父组件改变了`sonData`对应的值，子组件的`sonData`会立即改变，通过watch函数可以实时监听到值的变化
+  // `props`不属于data，但是`props`中的参数可以像data中的参数一样直接使用
+  props: ['sonData', 'businessData'],
+  data() {
+    return {
+      dialogVisible: false,
+      dialogTitle: '新增',
+      basePath: 'socialinsuAddr',
+      form: {
+        id: '',
+        sociInsuAreaNo: '',
+        sociInsuArea: '',
+        sociInsuTyp: ''
+      },
+      rules: {
+        sociInsuAreaNo: [{ required: true, trigger: 'blur', message: '请输入社保地区号' }]
+      }
+    }
+  },
+  watch: {
+    'sonData': function(newVal, oldVal) {
+      this.form = newVal
+      this.dialogVisible = true
+      if (newVal.id != null) {
+        this.dialogTitle = 'Edit'
+      } else {
+        this.dialogTitle = 'Add'
+      }
+    }
+  },
+  methods: {
+    _notify(message, type) {
+      this.$message({
+        message: message,
+        type: type
+      })
+    },
+    clearForm() {
+      this.form.id = null
+      this.form.sociInsuAreaNo = null
+      this.form.sociInsuArea = null
+      this.form.sociInsuTyp = null
+    },
+    handleClose() {
+      this.clearForm()
+      this.dialogVisible = false
+    },
+    onSubmit(form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          if (this.form.id === null) {
+            save(this.basePath, this.form).then(response => {
+              if (response.code === 200) {
+                this._notify(response.msg, 'success')
+                this.clearForm()
+                this.$emit('sonStatus', true)
+                this.dialogVisible = false
+              } else {
+                this._notify(response.msg, 'error')
+              }
+            })
+          } else {
+            edit(this.basePath, this.form).then(response => {
+              if (response.code === 200) {
+                this._notify(response.msg, 'success')
+                this.clearForm()
+                this.$emit('sonStatus', true)
+                this.dialogVisible = false
+              } else {
+                this._notify(response.msg, 'error')
+              }
+            })
+          }
+        } else {
+          this.$message('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .line {
+    text-align: center;
+  }
+
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
+
