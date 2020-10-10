5d72d9fdae4e4f86bcc6f860026ab0e08925bed8
liyjsimida <951250241@qq.com>
2020-10-10 16:55:01 +0800

    放大镜弹框

---
+++ src/views/param/dutySertypMatching/match.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-dialog title="解释码" :visible.sync="dialogTableVisible" append-to-body width="80%">
+    <proDuty dialog :param-type="paramType" @setMultipleSeleValues="setMultipleSeleValues" />
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="handleClose">
+        取消
+      </el-button>
+      <el-button type="primary" @click="onSubmit">
+        确认
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+<script>
+import proDuty from '../../productData/proDuty'
+export default {
+  name: 'Match',
+  components: { proDuty },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      basePath: 'proDuty',
+      listQuery: {
+        pageNum: 1,
+        pageSize: 10,
+        proDutyNme: '',
+        proDuty: '',
+        sort: '+id'
+      },
+      total: 0,
+      multipleSeleValues: [],
+      dialogTableVisible: false
+    }
+  },
+  watch: {
+    value(val) {
+      this.dialogTableVisible = val
+    },
+    dialogTableVisible(val) {
+      this.$emit('input', val)
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+    handleClose() {
+      this.dialogTableVisible = false
+    },
+    setMultipleSeleValues(value) {
+      this.multipleSeleValues = value
+    },
+    onSubmit() {
+      this.$emit('matchConfirm', this.multipleSeleValues)
+      this.dialogTableVisible = false
+    }
+  }
+}
+</script>
--- src/views/param/dutySertypMatching/save.vue
+++ src/views/param/dutySertypMatching/save.vue
@@ -2,8 +2,15 @@
   <el-dialog :title="dialogTitle" :before-close="handleClose" :visible.sync="dialogVisible" width="55%">
     <el-form ref="form" :inline="true" :rules="rules" :model="form" status-icon label-position="right" label-width="80px">
       <el-form-item label="责任号" prop="dutyNo" label-width="120px">
-        <el-input v-model="form.dutyNo" placeholder="请输入责任号" />
+        <el-input
+          v-model="form.dutyNo"
+          placeholder="请选择责任号"
+        >
+          <svg-icon slot="suffix" icon-class="search" @click="hanldeMatch" />
+        </el-input>
       </el-form-item>
+      <match v-model="matchVisable" @matchConfirm="matchConfirm" />
+
       <el-form-item label="责任说明" prop="dutyDesc" label-width="120px">
         <el-input v-model="form.dutyDesc" placeholder="请输入责任说明" />
       </el-form-item>
@@ -27,11 +34,15 @@
 
 <script>
 import { save, edit } from '@/api/base'
+import Match from './match'
 
 export default {
   // 父组件向子组件传值，通过props获取。
   // 一旦父组件改变了`sonData`对应的值，子组件的`sonData`会立即改变，通过watch函数可以实时监听到值的变化
   // `props`不属于data，但是`props`中的参数可以像data中的参数一样直接使用
+  components: {
+    Match
+  },
   props: ['sonData', 'businessData'],
   data() {
     return {
@@ -45,6 +56,7 @@ export default {
         begSertypCde: '',
         endSertypCde: ''
       },
+      matchVisable: false,
       rules: {
         dutyNo: [{ required: true, trigger: 'blur', message: '请输入责任号' }]
       }
@@ -78,6 +90,13 @@ export default {
     handleClose() {
       this.clearForm()
       this.dialogVisible = false
+    },
+    hanldeMatch() {
+      this.matchVisable = true
+    },
+    matchConfirm(data) {
+      this.form.dutyNo = data[0].proDuty
+      this.form.dutyDesc = data[0].proDutyNme
     },
     onSubmit(form) {
       this.$refs[form].validate((valid) => {
