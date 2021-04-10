b1405df4515ee4a4e6536be0b71d52d9c8044754
maycsimida <173626940@qq.com>
2021-04-10 22:16:56 +0800

    重点客户

---
+++ src/views/client/keyAccount/save.vue
@@ -0,0 +1,157 @@
+<template>
+  <el-dialog :title="dialogTitle" :before-close="handleClose" :visible.sync="dialogVisible" width="55%">
+    <el-form ref="form" :inline="true" :rules="rules" :model="form" status-icon label-position="right" label-width="80px">
+      <el-form-item label="团体号" prop="teamNo" label-width="120px">
+        <el-input v-model="form.teamNo" placeholder="请输入团体号" />
+      </el-form-item>
+      <el-form-item label="分单号" prop="sociInsuArea" label-width="120px">
+        <el-input v-model="form.plyPartNo" placeholder="请输入分单号" />
+      </el-form-item>
+      <el-form-item label="被保人ID" prop="insuredId" label-width="120px">
+        <el-input v-model="form.insuredId" placeholder="请输入被保人ID" />
+      </el-form-item>
+      <el-form-item label="保单号" prop="plyNo" label-width="120px">
+        <el-input v-model="form.plyNo" placeholder="请输入保单号" />
+      </el-form-item>
+
+      <el-form-item label="团体名称" prop="teamNme" label-width="120px">
+        <el-input v-model="form.teamNme" placeholder="请输入团体名称" />
+      </el-form-item>
+
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="handleClose">
+        Cancel
+      </el-button>
+      <el-button type="primary" @click="onSubmit('form')">
+        Confirm
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { save, edit } from '@/api/base'
+
+export default {
+  // 父组件向子组件传值，通过props获取。
+  // 一旦父组件改变了`sonData`对应的值，子组件的`sonData`会立即改变，通过watch函数可以实时监听到值的变化
+  // `props`不属于data，但是`props`中的参数可以像data中的参数一样直接使用
+  props: ['sonData', 'businessData'],
+  data() {
+    return {
+      dialogVisible: false,
+      dialogTitle: '新增',
+      basePath: 'keyAccount',
+      form: {
+        id: '',
+        teamNo: '',
+        plyPartNo: '',
+        insuredId: '',
+        plyNo: '',
+        teamNme: ''
+      },
+      rules: {
+        teamNo: [{ required: true, trigger: 'blur', message: '请输入团体号' }]
+      }
+    }
+  },
+  watch: {
+    'sonData': function(newVal, oldVal) {
+      this.form = newVal
+      this.dialogVisible = true
+      if (newVal.id != null) {
+        this.dialogTitle = 'Edit'
+      } else {
+        this.dialogTitle = 'Add'
+      }
+    }
+  },
+  methods: {
+    _notify(message, type) {
+      this.$message({
+        message: message,
+        type: type
+      })
+    },
+    clearForm() {
+      this.form.id = null
+      this.form.teamNo = null
+      this.form.plyPartNo = null
+      this.form.insuredId = null
+      this.form.plyNo = null
+      this.form.teamNme = null
+    },
+    handleClose() {
+      this.clearForm()
+      this.dialogVisible = false
+    },
+    onSubmit(form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          if (this.form.id === null) {
+            save(this.basePath, this.form).then(response => {
+              if (response.code === 200) {
+                this._notify(response.msg, 'success')
+                this.clearForm()
+                this.$emit('sonStatus', true)
+                this.dialogVisible = false
+              } else {
+                this._notify(response.msg, 'error')
+              }
+            })
+          } else {
+            edit(this.basePath, this.form).then(response => {
+              if (response.code === 200) {
+                this._notify(response.msg, 'success')
+                this.clearForm()
+                this.$emit('sonStatus', true)
+                this.dialogVisible = false
+              } else {
+                this._notify(response.msg, 'error')
+              }
+            })
+          }
+        } else {
+          this.$message('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .line {
+    text-align: center;
+  }
+
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
+
