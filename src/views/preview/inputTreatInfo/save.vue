c29d80428bf66d9118ddcebae718d85a91a3956c
wyqiand <1114640680@qq.com>
2021-05-13 13:10:57 +0800

    明细页面

--- src/views/preview/inputTreatInfo/save.vue
+++ src/views/preview/inputTreatInfo/save.vue
@@ -1,16 +1,69 @@
 <template>
   <el-dialog :title="dialogTitle" :before-close="handleClose" :visible.sync="dialogVisible" width="55%">
     <el-form ref="form" :inline="true" :rules="rules" :model="form" status-icon label-position="right" label-width="80px">
-      <el-form-item label="分单号" prop="plyPartNo" label-width="120px">
-        <el-input v-model="form.plyPartNo" placeholder="请输入分单号" />
-      </el-form-item>
 
       <el-form-item label="序号" prop="idx" label-width="120px">
         <el-input v-model="form.idx" placeholder="请输入序号" />
       </el-form-item>
 
-      <el-form-item label="批次号" prop="batchNo" label-width="120px">
-        <el-input v-model="form.batchNo" placeholder="请输入批次号" />
+      <el-form-item label="录入名称" prop="inputNme" label-width="120px">
+        <el-input v-model="form.inputNme" placeholder="请输入录入名称" />
+      </el-form-item>
+
+      <el-form-item label="录入剂型" prop="inputDrug" label-width="120px">
+        <el-input v-model="form.inputDrug" placeholder="请输入录入剂型" />
+      </el-form-item>
+
+      <el-form-item label="录入比例" prop="inputRate" label-width="120px">
+        <el-input v-model="form.inputRate" placeholder="请输入录入比例" />
+      </el-form-item>
+
+      <el-form-item label="录入医保类型" prop="inputSecuTyp" label-width="120px">
+        <el-input v-model="form.inputSecuTyp" placeholder="请输入录入医保类型" />
+      </el-form-item>
+
+      <el-form-item label="费用金额" prop="sumAmt" label-width="120px">
+        <el-input v-model="form.sumAmt" placeholder="请输入费用金额" />
+      </el-form-item>
+
+      <el-form-item label="费用编码" prop="treatCde" label-width="120px">
+        <el-input v-model="form.treatCde" placeholder="请输入费用编码" />
+      </el-form-item>
+
+      <el-form-item label="费用名称" prop="payNme" label-width="120px">
+        <el-input v-model="form.payNme" placeholder="请输入费用名称" />
+      </el-form-item>
+
+      <el-form-item label="医保地区" prop="medicalArea" label-width="120px">
+        <el-input v-model="form.medicalArea" placeholder="请输入医保地区" />
+      </el-form-item>
+
+      <el-form-item label="医保地省" prop="province" label-width="120px">
+        <el-input v-model="form.province" placeholder="请输入医保地省" />
+      </el-form-item>
+
+      <el-form-item label="医保地市" prop="city" label-width="120px">
+        <el-input v-model="form.city" placeholder="请输入医保地市" />
+      </el-form-item>
+
+      <el-form-item label="系统比例" prop="sysRate" label-width="120px">
+        <el-input v-model="form.sysRate" placeholder="请输入系统比例" />
+      </el-form-item>
+
+      <el-form-item label="扣费比例" prop="categSelfpayRate" label-width="120px">
+        <el-input v-model="form.categSelfpayRate" placeholder="请输入扣费比例" />
+      </el-form-item>
+
+      <el-form-item label="医保类型" prop="secuTyp" label-width="120px">
+        <el-input v-model="form.secuTyp" placeholder="请输入医保类型" />
+      </el-form-item>
+
+      <el-form-item label="自负金额" prop="categSelfpayAmt" label-width="120px">
+        <el-input v-model="form.categSelfpayAmt" placeholder="请输入自负金额" />
+      </el-form-item>
+
+      <el-form-item label="大项号" prop="maxtermNo" label-width="120px">
+        <el-input v-model="form.maxtermNo" placeholder="请输入大项号" />
       </el-form-item>
 
     </el-form>
